5d4e513a04a88eaa01f1973f39f0e9877091be3c
Angad Singh <angadsingh567890@gmail.com>
2019-02-02 16:09:35 +0530

    Updates

---
+++ precache-manifest.f04eae366b9ce368511a078348f4b984.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "49a00b2fef2525e2590e",
+    "url": "/minol/static/js/runtime~main.49a00b2f.js"
+  },
+  {
+    "revision": "b7bc995c5f96f7965b4d",
+    "url": "/minol/static/js/main.b7bc995c.chunk.js"
+  },
+  {
+    "revision": "1d183de65c855e050cda",
+    "url": "/minol/static/js/1.1d183de6.chunk.js"
+  },
+  {
+    "revision": "b7bc995c5f96f7965b4d",
+    "url": "/minol/static/css/main.ef22e58d.chunk.css"
+  },
+  {
+    "revision": "b1781702d321724a3aeb580b25b63ab3",
+    "url": "/minol/index.html"
+  }
+];
